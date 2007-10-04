a1342bbf8906350875f328f320e7a4db20f486fc
Soeren Sonnenburg <sonne@debian.org>
2007-10-04 22:47:34 +0000

    - remove links/faq/news and adjust css to make it useful for low resolutions - add sort by author/tag/title/license again (this time working...)

--- software/urls.py
+++ software/urls.py
@@ -18,15 +18,34 @@ base_generic_dict = {
     'paginate_by': 10,
     }
 
-
 software_info_dict = dict(base_generic_dict,
                           queryset=Software.objects.all(),
                           template_name='software_list.html')
 
-author_info_dict = dict(base_generic_dict,
-                        queryset=Author.objects.all(),
-                        template_name='author_list.html')
+software_info_dict_date = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('-pub_date'),
+		template_name='software_list.html')
 
+software_info_dict_software = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('-title'),
+		template_name='software_list.html')
+
+software_info_dict_author = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('-authors'),
+		template_name='software_list.html')
+
+software_info_dict_tags = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('-keyword1'),
+		template_name='software_list.html')
+
+software_info_dict_license = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('-os_license'),
+		template_name='software_list.html')
+
+#author_info_dict = dict(base_generic_dict,
+#                        queryset=Author.objects.all(),
+#                        template_name='author_list.html')
+#
 # General softwares views.
 urlpatterns = patterns('',
                        (r'^(?P<software_id>\d+)/$', entry.software_detail),
@@ -35,5 +54,9 @@ urlpatterns = patterns('',
 # Generic views.
 urlpatterns += patterns('',
                         (r'^$', object_list, software_info_dict),
-                        (r'^authors/$', object_list, author_info_dict),
+                        (r'^date/$', object_list, software_info_dict_date),
+                        (r'^software/$', object_list, software_info_dict_software),
+                        (r'^author/$', object_list, software_info_dict_author),
+                        (r'^tags/$', object_list, software_info_dict_tags),
+                        (r'^license/$', object_list, software_info_dict_license),
                         )
