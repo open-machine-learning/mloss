93c113c47080625e18d286ff9551297b0e1ed3ff
Soeren Sonnenburg <sonne@debian.org>
2007-10-05 12:23:33 +0000

    first attempts for authentification/edit

--- software/forms.py
+++ software/forms.py
@@ -49,6 +49,8 @@ def addsoftware(request):
             #filename = object.user.username + '_' + filename
             object.save_tarball_file(filename, request.FILES['tarball']['content'])
 
+	if not request.user.is_authenticated:
+		raise HttpResponseServerError
     
     original_id = request.GET.get('oid', None)
     if request.method == 'POST':
@@ -85,4 +87,56 @@ def addsoftware(request):
     return render_to_response('software_add.html',
                               { 'form': form },
                               context_instance=RequestContext(request))
-#addsoftware = login_required(addsoftware)
+
+def editsoftware(request):
+    """
+    Show the software form, fill with content and capture the information
+    """
+
+    def save_tarball(object):
+        """
+        Retrieve filename and save the file
+        """
+        if request.FILES.has_key('tarball'):
+            filename = request.FILES['tarball']['filename']
+            #filename = object.user.username + '_' + filename
+            object.save_tarball_file(filename, request.FILES['tarball']['content'])
+
+	if not request.user.is_authenticated:
+		raise HttpResponseServerError
+    
+    original_id = request.GET.get('oid', None)
+    if request.method == 'POST':
+        #post_data = request.POST.copy()
+        #post_data.update(request.FILES)
+        form = AddSoftwareForm(request.POST)
+        if form.is_valid():
+            # somehow the default behaviour doesn't work with file uploads
+            #new_software = Software(form)
+            new_software = Software(title=form.cleaned_data['title'],
+                                    authors=form.cleaned_data['authors'],
+                                    description=form.cleaned_data['description'],
+                                    project_url=form.cleaned_data['project_url'],
+                                    pub_date=datetime.datetime.now(),
+                                    updated_date=datetime.datetime.now(),
+                                    keyword1=form.cleaned_data['keyword1'],
+                                    keyword2=form.cleaned_data['keyword2'],
+                                    keyword3=form.cleaned_data['keyword3'],
+                                    keyword4=form.cleaned_data['keyword4'],
+                                    keyword5=form.cleaned_data['keyword5'],
+                                    language=form.cleaned_data['language'],
+                                    os_license=form.cleaned_data['os_license'],
+                                    tarball=form.cleaned_data['tarball'],
+                                    )
+            if original_id:
+                new_software.original_id = original_id
+            save_tarball(new_software)
+            new_software.save()
+            return HttpResponseRedirect(new_software.get_absolute_url())
+        else:
+            print form
+    else:
+        form = AddSoftwareForm()
+    return render_to_response('software_add.html',
+                              { 'form': form },
+                              context_instance=RequestContext(request))
