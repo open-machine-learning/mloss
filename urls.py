907d418d3db1f2501ad32f460154139be6e0b4f6
Soeren Sonnenburg <sonne@debian.org>
2007-10-14 22:23:50 +0000

    try to be more stylish

--- urls.py
+++ urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from django.shortcuts import render_to_response
-from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('',
     # administration
@@ -15,11 +13,12 @@ urlpatterns = patterns('',
     (r'^community/', include('community.urls')),
 
     # Display News and FAQ- simplest possible dynamic page
-    #(r'^news/', 'forshow.views.newsindex'),
+    (r'^news/', 'forshow.views.newsindex'),
     (r'^faq/', 'forshow.views.faqindex'),
 
     # redirect the root to news
-    ('^$', 'forshow.views.newsindex'),
+    #('^$', 'forshow.views.newsindex'),
+    ('^$', 'django.views.generic.simple.redirect_to', {'url':'/about/'}),
 
     # Enable comments
     (r'^comments/', include('django.contrib.comments.urls.comments')),
