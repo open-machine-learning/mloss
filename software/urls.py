896fbdbaf4c03b6b27ab6d4dfb5463f3111a08e5
Soeren Sonnenburg <sonne@debian.org>
2007-10-15 05:40:32 +0000

    further beatyfications

--- software/urls.py
+++ software/urls.py
@@ -23,19 +23,23 @@ software_info_dict_date = dict(base_generic_dict,
 		template_name='software/software_list.html')
 
 software_info_dict_software = dict(base_generic_dict,
-		queryset=Software.objects.all().order_by('-title'),
+		queryset=Software.objects.all().order_by('title'),
 		template_name='software/software_list.html')
 
 software_info_dict_author = dict(base_generic_dict,
-		queryset=Software.objects.all().order_by('-authors'),
+		queryset=Software.objects.all().order_by('authors'),
+		template_name='software/software_list.html')
+
+software_info_dict_submitter = dict(base_generic_dict,
+		queryset=Software.objects.all().order_by('user'),
 		template_name='software/software_list.html')
 
 software_info_dict_tags = dict(base_generic_dict,
-		queryset=Software.objects.all().order_by('-tags'),
+		queryset=Software.objects.all().order_by('tags'),
 		template_name='software/software_list.html')
 
 software_info_dict_license = dict(base_generic_dict,
-		queryset=Software.objects.all().order_by('-os_license'),
+		queryset=Software.objects.all().order_by('os_license'),
 		template_name='software/software_list.html')
 
 # General softwares views.
@@ -45,6 +49,7 @@ urlpatterns = patterns('',
                         (r'^date/$', object_list, software_info_dict_date),
                         (r'^title/$', object_list, software_info_dict_software),
                         (r'^author/$', object_list, software_info_dict_author),
+                        (r'^submitter/$', object_list, software_info_dict_submitter),
                         (r'^tags/$', object_list, software_info_dict_tags),
                         (r'^license/$', object_list, software_info_dict_license),
 						(r'^submit/', 'software.forms.add_software'),
