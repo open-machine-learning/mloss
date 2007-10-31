74be8b87a21c18193ac6946dca9e54b3582d9583
Soeren Sonnenburg <sonne@debian.org>
2007-10-31 09:57:43 +0000

    convert forum to use new forms and add proper authorization fields to enable quicker posts. also add some validation (one could create empty posts before 		*sigh*). logic is now the following: if a form for some reason fails validation not the whole thread list is shown but just the thread with the latest post (same for reply / only latest reply is shown)

--- community/views.py
+++ community/views.py
@@ -9,6 +9,94 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django import newforms as forms
+from django.contrib.auth import authenticate, login
+
+class NewPostForm(forms.Form):
+	body = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":80}))
+
+class NewPostFormwPassword(forms.Form):
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'size':'20'}), required=True, max_length=40)
+	username = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}), required=True, max_length=40)
+	body = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":80}))
+
+	def clean_username(self):
+		if 'username' not in self.cleaned_data:
+			raise forms.ValidationError(u'This field is required.')
+		if 'username' in self.data and 'password' in self.data:
+			username=self.cleaned_data['username']
+			password=self.cleaned_data['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					self.user = user
+				else:
+					raise forms.ValidationError(u'User Account disabled.')
+			else:
+				raise forms.ValidationError(u'Please enter a correct username and password. Note that both fields are case-sensitive.')
+
+	def login(self, request):
+		if self.is_valid():
+			login(request, self.user)
+			return request.user.is_authenticated()
+		return False
+
+def create_newpostform(request):
+	if request.user.is_authenticated():
+		if request.method == 'POST':
+			inputform = NewPostForm(request.POST)
+		else:
+			inputform = NewPostForm()
+	else:
+		if request.method == 'POST':
+			inputform = NewPostFormwPassword(request.POST)
+		else:
+			inputform = NewPostFormwPassword()
+	return inputform
+
+class NewThreadForm(forms.Form):
+	body = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":80}))
+	title = forms.CharField(widget=forms.TextInput(attrs={'size':'40'}), required=True, max_length=100)
+
+class NewThreadFormwPassword(forms.Form):
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'size':'20'}), required=True, max_length=40)
+	username = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}), required=True, max_length=40)
+	title = forms.CharField(widget=forms.TextInput(attrs={'size':'40'}), required=True, max_length=100)
+	body = forms.CharField(widget=forms.Textarea(attrs={"rows":10, "cols":80}))
+
+	def clean_username(self):
+		if 'username' not in self.cleaned_data:
+			raise forms.ValidationError(u'This field is required.')
+		if 'username' in self.data and 'password' in self.data:
+			username=self.cleaned_data['username']
+			password=self.cleaned_data['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					self.user = user
+				else:
+					raise forms.ValidationError(u'User Account disabled.')
+			else:
+				raise forms.ValidationError(u'Please enter a correct username and password. Note that both fields are case-sensitive.')
+
+	def login(self, request):
+		if self.is_valid():
+			login(request, self.user)
+			return True
+		return False
+
+def create_newthreadform(request):
+	if request.user.is_authenticated():
+		if request.method == 'POST':
+			inputform = NewThreadForm(request.POST)
+		else:
+			inputform = NewThreadForm()
+	else:
+		if request.method == 'POST':
+			inputform = NewThreadFormwPassword(request.POST)
+		else:
+			inputform = NewThreadFormwPassword()
+	return inputform
+
 
 def forum(request, slug):
 	"""
@@ -18,9 +106,13 @@ def forum(request, slug):
 	"""
 	f = get_object_or_404(Forum, slug=slug)
 
+	inputform = create_newthreadform(request)
+
 	return render_to_response('community/thread_list.html',
 		RequestContext(request, {
 			'forum': f,
+			'form': inputform,
+			'form_action' : 'new/',
 			'threads': f.thread_set.all()
 		}))
 
@@ -35,34 +127,17 @@ def thread(request, forum, thread):
 
 	t.views += 1
 	t.save()
+
+	inputform = create_newpostform(request)
 	
 	return render_to_response('community/thread.html',
 		RequestContext(request, {
 			'forum': f,
+			'form': inputform,
 			'thread': t,
 			'posts': p,
+			'form_action' : 'reply/'
 		}))
-
-def reply(request, forum, thread):
-	"""
-	If a thread isn't closed, and the user is logged in, post a reply
-	to a thread. Note we don't have "nested" replies at this stage.
-	"""
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect('/accounts/login?next=%s' % request.path)
-	f = get_object_or_404(Forum, slug=forum)
-	t = get_object_or_404(Thread, pk=thread)
-	if t.closed:
-		return HttpResponseRedirect('/accounts/login?next=%s' % request.path)
-	body = request.POST.get('body', False)
-	p = Post(
-		thread=t, 
-		author=request.user,
-		body=body,
-		time=datetime.now(),
-		)
-	p.save()
-	return HttpResponseRedirect(p.get_absolute_url())
 
 def newthread(request, forum):
 	"""
@@ -72,19 +147,73 @@ def newthread(request, forum):
 
 	Only allows a user to post if they're logged in.
 	"""
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect('/accounts/login?next=%s' % request.path) 
 	f = get_object_or_404(Forum, slug=forum)
-	t = Thread(
-		forum=f,
-		title=request.POST.get('title'),
-	)
-	t.save()
-	p = Post(
-		thread=t,
-		author=request.user,
-		body=request.POST.get('body'),
-		time=datetime.now(),
-	)
-	p.save()
-	return HttpResponseRedirect(t.get_absolute_url())
+	t = f.thread_set.all().order_by('thread_latest_post')[:1]
+
+	if request.method == 'POST':
+		form = create_newthreadform(request)
+		if form.is_valid():
+			print "form valid"
+			if request.user.is_authenticated() or form.login(request):
+				t = Thread(
+					forum=f,
+					title=form.cleaned_data['title'],
+				)
+				t.save()
+				p = Post(
+					thread=t,
+					author=request.user,
+					body=form.cleaned_data['body'],
+					time=datetime.now(),
+				)
+				p.save()
+				return HttpResponseRedirect(t.get_absolute_url())
+		else:
+			print "form not valid"
+	else:
+		form = create_newthreadform(request)
+	
+	return render_to_response('community/thread_list.html',
+			RequestContext(request, {
+			'forum': f,
+			'form': form,
+			'form_action' : '',
+			'threads': t
+			}))
+
+def reply(request, forum, thread):
+	"""
+	If a thread isn't closed, and the user is logged in, post a reply
+	to a thread. Note we don't have "nested" replies at this stage.
+	"""
+	f = get_object_or_404(Forum, slug=forum)
+	t = get_object_or_404(Thread, pk=thread)
+	p = t.post_set.all().order_by('-time')[:1]
+
+	if t.closed:
+		return HttpResponseRedirect('/accounts/login?next=%s' % request.path)
+
+	if request.method == 'POST':
+		form = create_newpostform(request)
+		if form.is_valid():
+			if request.user.is_authenticated() or form.login(request):
+				p = Post(
+					thread=t, 
+					author=request.user,
+					body=form.cleaned_data['body'],
+					time=datetime.now(),
+					)
+				p.save()
+				return HttpResponseRedirect(p.get_absolute_url())
+	else:
+		form = create_newpostform(request)
+
+	return render_to_response('community/thread.html',
+			RequestContext(request, {
+			'forum': f,
+			'thread': t,
+			'posts': p,
+			'form': form,
+			'form_action' : ''
+			}))
+
