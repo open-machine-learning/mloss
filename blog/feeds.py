90f3f3b0f4407b7b827c22d7939bfdc90724f492
Soeren Sonnenburg <sonne@debian.org>
2008-05-04 14:32:31 +0000

    add a feed to the community page, needs to be revised though

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,11 +1,27 @@
-from django.contrib.syndication.feeds import Feed
+from wfwfeed import WellFormedWebRss
 from blog.models import BlogItem
-import datetime
+from django.contrib.sites.models import Site
+from django.http import HttpResponse
+from markdown import markdown
 
-class WeblogEntryFeed(Feed):
-    title = "The mloss weblog"
-    link = "http://mloss.org/weblog"
-    description = "Some thoughts about machine learning open source software"
+def RssBlogFeed(request):
+    try:
+        object_list = BlogItem.objects.all().order_by('-pub_date')[:10]
+    except documents.DocumentDoesNotExist:
+        raise Http404
+    feed = WellFormedWebRss( u"The mloss.org community blog",
+            "http://mloss.org/community",
+            u'Some thoughts about machine learning open source software',
+            language=u"en")
 
-    def items(self):
-        return BlogItem.objects.filter(pub_date__lte=datetime.datetime.now())[:10]
+    for object in object_list:
+        link = 'http://%s%s' % (Site.objects.get_current().domain, object.get_absolute_url())
+        commentlink=u'http://%s/software/rss/comments/%i' % (Site.objects.get_current().domain, object.id)
+        feed.add_item( object.headline.encode('utf-8'),
+                link, markdown(object.body),
+                author_name=object.author.encode('utf-8'),
+                comments=commentlink,
+                pubdate=object.pub_date, unique_id=link)
+    response = HttpResponse(mimetype='application/xml')
+    feed.write(response, 'utf-8')
+    return response
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from blog.models import BlogItem
+from blog.feeds import RssBlogFeed
 
 info_dict = {
     'queryset' : BlogItem.objects.all(),
@@ -11,4 +12,5 @@ urlpatterns = patterns('django.views.generic.date_based',
    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 'archive_day', info_dict),
    (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 'archive_month', info_dict),
    (r'^(?P<year>\d{4})/$', 'archive_year', info_dict),
+   (r'^rss/latest/$', RssBlogFeed),
 )
