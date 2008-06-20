3c0e6dd4d2967ac975cd6e8932e5d17cd6a206e0
Cheng Soon Ong <chengsoon.ong@inf.ethz.ch>
2008-06-20 13:36:52 +0000

    Can now slurp from CRAN. Cannot figure out the logo magic, but it would be nice to have the R logo for those projects that have been slurped.

--- cran/update_cran.py
+++ cran/update_cran.py
@@ -1,6 +1,7 @@
 import urllib
 from xml.dom import minidom
 from software.models import Software
+from django.contrib.auth.models import User
 
 
 
@@ -11,20 +12,82 @@ class CRANPackage:
     """
     name = ""
     url = ""
-    description = ""
     description_url = ""    
     is_core = False
+    cran_text = ""
+
+    decription = ""
+    os_license = ""
+    version = ""
 
     def __init__(self,node_value):
         self.name = node_value
         self.url = 'http://cran.r-project.org/package=' + self.name
         self.description_url = 'http://cran.r-project.org/web/packages/'+self.name+'/DESCRIPTION'
 
-    def get_description():
+    def __repr__(self):
+        return (self.name, self.url, self.cran_text, self.description_url, self.is_core)
+
+    def parse_cran_text(self):
+        """
+        Read the data at description_url and save it to description
+        Parse the text description from CRAN (in debian control file format)
+        and return a dictionary.
+        """
         usock = urllib.urlopen(self.description_url)
-        self.description = usock.read()
+        self.cran_text = usock.read()
         usock.close()
-        
+
+        cran_dict = {}
+        all_lines = self.cran_text.split('\n')
+        curkey = None
+        for line in all_lines:
+            if line != '' and (line[0] == ' ' or line[0] == '\t'):
+                curval += ' ' + line.strip()
+            else:
+                if curkey is not None:
+                    cran_dict[curkey] = curval
+                if line == '':
+                    continue
+                kv = line.split(':')
+                curkey = kv[0]
+                curval = kv[1].strip()   # this doesn't work for url
+                
+        self.os_license = cran_dict['License'].strip()
+        self.version = cran_dict['Version'].strip()
+        if cran_dict.has_key('Title'):
+            self.description = cran_dict['Title'] + ': ' + cran_dict['Description']
+        else:
+            self.description = cran_dict['Bundle'] + ': ' + cran_dict['BundleDescription']
+
+    def insert_into_database(self):
+        """
+        For a given CRANPackage construct Software object and insert it into database
+        """
+        # check whether the package exists
+        dbpkg = Software.objects.filter(title=self.name)
+        if dbpkg.count() == 0:
+            # Hard code that Cheng submits software.
+            me = User.objects.filter(username='ong')[0]
+
+            # if not create a new Software project
+            spkg = Software(user=me, title=self.name, version=self.version,
+                            description=self.description, os_license=self.os_license,
+                            language='R', operating_systems='agnostic',
+                            download_url=self.url, tags='auto from cran')
+        else:
+            print 'Package ' + self.name + ' found, UPDATING...'
+            assert(dbpkg.count() == 1)
+            spkg = dbpkg[0]
+            # Don't mess around with manual entries.
+            if spkg.tags == 'auto from cran':
+                return
+            spkg.version = self.version
+            spkg.description = self.description
+            spkg.os_license = self.os_license
+            spkg.download_url = self.url
+        spkg.save()
+
 
 def parse_ctv():
     """
@@ -51,15 +114,10 @@ def parse_ctv():
     return cpkglist
         
 
-def insert_into_database(cpkg):
-    """
-    For a given CRANPackage construct Software object and insert it into database
-    """
-    # How to deal with: user
-    spkg = Software(title=cpkg.name, description=cpkg.get_description(),
-                    language='R', operating_systems='agnostic',
-                    download_url=cpkg.url)
-    spkg.save()
-
-
-    
+def main():
+    """Slurp projects from CRAN"""
+    cpkglist = parse_ctv()
+    for pkg in cpkglist:
+        print pkg.name
+        pkg.parse_cran_text()
+        pkg.insert_into_database()
