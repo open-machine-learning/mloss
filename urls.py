fa5a568e5cadf9ce005a49e7c2857746bff62c72
Dominik Kühne <dominik.kuehne@tu-berlin.de>
2018-08-28 12:15:48 +0200

    django 1.4 update

--- urls.py
+++ urls.py
@@ -8,27 +8,40 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # administration
-    (r'^admin/(.*)', admin.site.root),
+    #(r'^admin/(.*)', admin.site.root),
     #(r'^admin/', include(admin.site.urls)),
+	
+    url(r'^admin/', include(admin.site.urls)),
 
     # software and revision
+    #(r'^software/', include('software.urls')),
+    #(r'^revision/', include('revision.urls')),
     (r'^software/', include('software.urls')),
     (r'^revision/', include('revision.urls')),
+   
 
     # Using registration
     (r'^accounts/', include('registration.backends.default.urls')),
     (r'^community/', include('community.urls')),
     (r'^user/', include('user.urls')),
+    #(r'^accounts/', include('registration.backends.default.urls')),
+    #(r'^community/', include('community.urls')),
+    #(r'^user/', include('user.urls')),
+
 
     # Display News and FAQ- simplest possible dynamic page
+    #(r'^news/', 'forshow.views.newsindex'),
+    #(r'^faq/', 'forshow.views.faqindex'),
     (r'^news/', 'forshow.views.newsindex'),
     (r'^faq/', 'forshow.views.faqindex'),
 
     # redirect the root to news
     #('^$', 'forshow.views.newsindex'),
+    #('^$', 'django.views.generic.simple.redirect_to', {'url':'/software/'}),
     ('^$', 'django.views.generic.simple.redirect_to', {'url':'/software/'}),
 
     # Enable comments
+    #(r'^comments/', include('django.contrib.comments.urls')),
     (r'^comments/', include('django.contrib.comments.urls')),
 )
 
